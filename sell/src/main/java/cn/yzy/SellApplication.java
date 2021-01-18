27592931ee59ef60be4d8d3f729973bc195c2cb8
wangyifan123 <727804482@qq.com>
2021-01-19 01:13:53 +0800

    20200119修改

--- sell/src/main/java/cn/yzy/SellApplication.java
+++ sell/src/main/java/cn/yzy/SellApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SellApplication {
 
     public static void main(String[] args) {
+
+        System.out.println("修改");
         SpringApplication.run(SellApplication.class, args);
     }
 
